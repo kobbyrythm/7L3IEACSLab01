668e8a58b9755a6b6df44e6090fcc8db6e549126
Isaac Laryea <62596396+kobbyrythm@users.noreply.github.com>
2020-04-05 17:25:14 +0200

    Update problem1.cc

--- QtProject/labs1/problem1.cc
+++ QtProject/labs1/problem1.cc
@@ -2,9 +2,55 @@
 
 #include "problem1.h"
 
-
-
-void example_function() {
-    std::cout << "Call functiopn example_function()" << std::endl;
-
+//SWAP
+void SwapValue(int a, int b) {
+   int t = a;
+   a = b;
+   b = t;
 }
+
+
+//SAME SIGN
+  bool sameSign(int x, int y)
+
+  {  if (x >=0 && y < 0||x < 0 && y >= 0)
+         std::cout<<" different sign";
+         else
+         std::cout<<"same sign";
+
+
+    return (x >= 0) ^ (y < 0);
+
+
+
+//EVEN OR ODD
+bool EvenOdd(int num){
+   bool b;
+
+   if (num % 2 == 0)
+      b=true;
+   else
+      b=false;
+
+   return b;
+}
+
+
+
+
+//FIRST OR SECOND
+int first()
+{
+    for(int i =0;i<3;i++)
+
+    std::cout<<"First\n";
+    return 0;
+}
+
+int second()
+{
+    for(int i =0;i<2;i++)
+
+    std::cout<<"Second\n";
+  }
+    
