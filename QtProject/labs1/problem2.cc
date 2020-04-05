02b6a004b95623a11326e096cec00efcc1c92c10
Isaac Laryea <62596396+kobbyrythm@users.noreply.github.com>
2020-04-05 17:27:26 +0200

    Update problem2.cc

--- QtProject/labs1/problem2.cc
+++ QtProject/labs1/problem2.cc
@@ -1,1 +1,10 @@
 #include "problem2.h"
+void function()
+ {
+ 	double function;
+ 	for(int i=1 ;i <=10;i++)
+ 	{
+ 	function=sin(i)+log(i);
+ 	std::cout<<"x range from[1 10] :"<< function<<std::endl;
+ 	}
+  }
