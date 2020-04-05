484935dd6a0964218d341947f135c65e58fd8b84
Isaac Laryea <62596396+kobbyrythm@users.noreply.github.com>
2020-04-05 17:50:31 +0200

    Update problem3.cc

--- QtProject/labs1/problem3.cc
+++ QtProject/labs1/problem3.cc
@@ -1,1 +1,15 @@
 #include "problem3.h"
+
+void swap(int * a, int * b) {
+    int temp = * a;
+    * a = * b;
+    * b = temp;
+}
+
+void swap(int & a, int & b) {
+    int temp =  a;
+    a = b;
+    b = temp;
+}
+
+}
