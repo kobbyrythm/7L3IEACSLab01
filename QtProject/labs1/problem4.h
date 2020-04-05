49ff242618aced7ba2ca6193bde331ad8e2faf20
Isaac Laryea <62596396+kobbyrythm@users.noreply.github.com>
2020-04-05 17:29:50 +0200

    Update problem4.h

--- QtProject/labs1/problem4.h
+++ QtProject/labs1/problem4.h
@@ -1,4 +1,22 @@
 #ifndef PROBLEM4_H
 #define PROBLEM4_H
 
+//BUBBLE SORT
+void bubble_sort(int[]);
+
+
+void bubble_sort(int fn_arr[]) {
+
+  int i, j, a, t;
+};
+
+//QUICK SORT
+
+int partition(int *a,int start,int end);
+int pivot=a[end];
+
+
+int P_index=start;
+int i,t;
+void Quicksort(int *a,int start,int end);
 #endif // PROBLEM4_H
