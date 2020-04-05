1564a41d396a3eca440db5fd8733bacc59e021b1
Isaac Laryea <62596396+kobbyrythm@users.noreply.github.com>
2020-04-05 17:30:50 +0200

    Update problem5.cc

--- QtProject/labs1/problem5.cc
+++ QtProject/labs1/problem5.cc
@@ -1,1 +1,27 @@
 #include "problem5.h"
+ void transpose(int matrix[10][10], int rows, int cols);
+
+
+void printPascal(int n)
+{
+
+	for (int line = 1; line <= n; line++)
+	{
+
+		int C = 1;
+
+		for (int i = 1; i < (n - line + 1); i++){
+			std::cout << " ";
+		}
+
+		for (int i = 1; i <= line; i++)
+		{
+
+
+			std::cout << C << " ";
+			C = C * (line - i) / i;
+		}
+
+		std::cout << "\n";
+	}
+}
